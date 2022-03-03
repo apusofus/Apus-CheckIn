78cbf82d7d2c1482a63ef5af210d24df116209b1
JaemooJung <dbdbwer@naver.com>
2022-03-03 17:50:25 +0900

    Modal View Done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- ApusCheckIn/ApusCheckIn/View/MyModalView.swift
+++ ApusCheckIn/ApusCheckIn/View/MyModalView.swift
@@ -11,76 +11,78 @@ struct MyModalView: View {
     
     //뷰모델의 클래스를 @ObservedObject 변수명 viewModel로 선언한다.
     @ObservedObject var viewModel: MyModalViewModel
+    let date: Date
     
-
     var body: some View {
         NavigationView {
-                VStack {
-                    List {
-                        Section {
-                            //여기서 ForEach를 쓰려면 Model ->idenfiable
-                            //VieMode -> private(set)으로 해야함
-                            ForEach(viewModel.members) { member in
-                                if member.didMemberCheckInToday() {
-                                    HStack() {
-                                        Text("\(member.name)")
-                                        Spacer()
-                                        Text("time")
-                                            .font(.caption)
-                                            .foregroundColor(Color.gray)
-                                        //이미지를 뷰로 따로 빼기. 템플릿화
-                                        if member.didMemberCheckInThur() {
-                                            Image(systemName: "swift")
-                                                .foregroundColor(.blue)
-                                        } else {
-                                            Image(systemName: "applelogo")
-                                                .foregroundColor(.blue)
-                                                
-                                                
-                                        }
-
-
+            VStack {
+                List {
+                    Section {
+                        //여기서 ForEach를 쓰려면 Model ->idenfiable
+                        //VieMode -> private(set)으로 해야함
+                        ForEach(viewModel.members) { member in
+                            if member.didMemberCheckInOnTheDate(theDate: self.date) {
+                                HStack() {
+                                    Text("\(member.intraID)")
+                                    Spacer()
+                                    Text("time")
+                                        .font(.caption)
+                                        .foregroundColor(Color.gray)
+                                    //이미지를 뷰로 따로 빼기. 템플릿화
+                                    if member.didMemberCheckInThur() {
+                                        Image(systemName: "swift")
+                                            .foregroundColor(.blue)
+                                    } else {
+                                        Image(systemName: "applelogo")
+                                            .foregroundColor(.blue)
+                                        
+                                        
                                     }
+                                    
+                                    
                                 }
- 
                             }
-                        } header: {
-                            Text("APUS On")
-                                .foregroundColor(.blue)
-
+                            
                         }
+                    } header: {
+                        Text("APUS On")
+                            .foregroundColor(.blue)
                         
-                        
-                        Section {
-                            ForEach(viewModel.members) { member in
-                                if member.didMemberCheckInToday() == false {
-                                    HStack() {
-                                        Text("\(member.name)")
-                                            .foregroundColor(.gray)
-                                     }
-                                }
- 
-                            }
-                        } header: {
-                            Text("APUS Off")
-                                .foregroundColor(.gray)
-                        }
                     }
-                    .listStyle(SidebarListStyle())
-
+                    
+                    
+                    Section {
+                        ForEach(viewModel.members) { member in
+                            if member.didMemberCheckInOnTheDate(theDate: self.date) == false {
+                                HStack() {
+                                    Text("\(member.intraID)")
+                                        .foregroundColor(.gray)
+                                }
+                            }
+                            
+                        }
+                    } header: {
+                        Text("APUS Off")
+                            .foregroundColor(.gray)
+                    }
                 }
-                .navigationBarTitle(Member.changeDateToString())
+                .listStyle(SidebarListStyle())
+                
+            }
+            .navigationBarTitle(Member.changeDateToString(theDate: self.date))
         }
-
+        
     }
+}
+
+extension MyModalView {
+    
 }
 
 
 
-                           
-
 struct MyModalView_Previews: PreviewProvider {
     static var previews: some View {
-        MyModalView(viewModel: MyModalViewModel())
+        MyModalView(viewModel: MyModalViewModel(), date: Date())
     }
 }
